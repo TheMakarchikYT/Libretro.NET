e7263cb76a3ad43ca2a1741fa1a9c7ef3c539c90
TheMakarchikYT <makarchikx@yandex.ru>
2026-02-22 22:42:34 +0300

    first commit

--- Libretro.NET/Helpers/NativeDispatchProxy.cs
+++ Libretro.NET/Helpers/NativeDispatchProxy.cs
@@ -19,11 +19,37 @@ namespace Libretro.NET
         private IntPtr _lib;
         private List<Delegate> _delegates = new List<Delegate>();
 
+        private SetEnvironmentFunc _setEnvironment;
+        private SetVideoRefreshFunc _setVideoRefresh;
+        private SetInputPollFunc _setInputPoll;
+        private SetInputStateFunc _setInputState;
+        private SetAudioSampleFunc _setAudioSample;
+        private SetAudioSampleBatchFunc _setAudioSampleBatch;
+        private InitFunc _init;
+        private DeinitFunc _deinit;
+        private GetSystemInfoFunc _getSystemInfo;
+        private LoadGameFunc _loadGame;
+        private GetSystemAvInfoFunc _getSystemAvInfo;
+        private RunFunc _run;
+
         public void Load(string path)
         {
             _lib = LoadLibrary(path);
             if (_lib == IntPtr.Zero)
                 throw new Exception("Failed to load core: " + path);
+
+            _setEnvironment = GetFunc<SetEnvironmentFunc>("retro_set_environment");
+            _setVideoRefresh = GetFunc<SetVideoRefreshFunc>("retro_set_video_refresh");
+            _setInputPoll = GetFunc<SetInputPollFunc>("retro_set_input_poll");
+            _setInputState = GetFunc<SetInputStateFunc>("retro_set_input_state");
+            _setAudioSample = GetFunc<SetAudioSampleFunc>("retro_set_audio_sample");
+            _setAudioSampleBatch = GetFunc<SetAudioSampleBatchFunc>("retro_set_audio_sample_batch");
+            _init = GetFunc<InitFunc>("retro_init");
+            _deinit = GetFunc<DeinitFunc>("retro_deinit");
+            _getSystemInfo = GetFunc<GetSystemInfoFunc>("retro_get_system_info");
+            _loadGame = GetFunc<LoadGameFunc>("retro_load_game");
+            _getSystemAvInfo = GetFunc<GetSystemAvInfoFunc>("retro_get_system_av_info");
+            _run = GetFunc<RunFunc>("retro_run");
         }
 
         private TDelegate GetFunc<TDelegate>(string name) where TDelegate : class
@@ -44,71 +70,74 @@ namespace Libretro.NET
 
         public void set_environment(retro_environment_t cb)
         {
-            _delegates.Add(cb);
-            GetFunc<SetEnvironmentFunc>("retro_set_environment")(Marshal.GetFunctionPointerForDelegate(cb));
+            EnvironmentBridge bridge = (uint cmd, IntPtr data) => cb(cmd, (void*)data) ? 1 : 0;
+            _delegates.Add(bridge);
+            _setEnvironment(Marshal.GetFunctionPointerForDelegate(bridge));
         }
 
         public void set_video_refresh(retro_video_refresh_t cb)
         {
-            _delegates.Add(cb);
-            GetFunc<SetVideoRefreshFunc>("retro_set_video_refresh")(Marshal.GetFunctionPointerForDelegate(cb));
+            VideoRefreshBridge bridge = (IntPtr data, uint width, uint height, UIntPtr pitch) => cb((void*)data, width, height, pitch);
+            _delegates.Add(bridge);
+            _setVideoRefresh(Marshal.GetFunctionPointerForDelegate(bridge));
         }
 
         public void set_input_poll(retro_input_poll_t cb)
         {
             _delegates.Add(cb);
-            GetFunc<SetInputPollFunc>("retro_set_input_poll")(Marshal.GetFunctionPointerForDelegate(cb));
+            _setInputPoll(Marshal.GetFunctionPointerForDelegate(cb));
         }
 
         public void set_input_state(retro_input_state_t cb)
         {
             _delegates.Add(cb);
-            GetFunc<SetInputStateFunc>("retro_set_input_state")(Marshal.GetFunctionPointerForDelegate(cb));
+            _setInputState(Marshal.GetFunctionPointerForDelegate(cb));
         }
 
         public void set_audio_sample(retro_audio_sample_t cb)
         {
             _delegates.Add(cb);
-            GetFunc<SetAudioSampleFunc>("retro_set_audio_sample")(Marshal.GetFunctionPointerForDelegate(cb));
+            _setAudioSample(Marshal.GetFunctionPointerForDelegate(cb));
         }
 
         public void set_audio_sample_batch(retro_audio_sample_batch_t cb)
         {
-            _delegates.Add(cb);
-            GetFunc<SetAudioSampleBatchFunc>("retro_set_audio_sample_batch")(Marshal.GetFunctionPointerForDelegate(cb));
+            AudioSampleBatchBridge bridge = (IntPtr data, UIntPtr frames) => cb((short*)data, frames);
+            _delegates.Add(bridge);
+            _setAudioSampleBatch(Marshal.GetFunctionPointerForDelegate(bridge));
         }
 
         public void init()
         {
-            GetFunc<InitFunc>("retro_init")();
+            _init();
         }
 
         public void get_system_info(ref retro_system_info info)
         {
             fixed (retro_system_info* p = &info)
-                GetFunc<GetSystemInfoFunc>("retro_get_system_info")(p);
+                _getSystemInfo((IntPtr)p);
         }
 
         public byte load_game(ref retro_game_info game)
         {
             fixed (retro_game_info* p = &game)
-                return GetFunc<LoadGameFunc>("retro_load_game")(p);
+                return _loadGame(p);
         }
 
         public void get_system_av_info(ref retro_system_av_info av)
         {
             fixed (retro_system_av_info* p = &av)
-                GetFunc<GetSystemAvInfoFunc>("retro_get_system_av_info")(p);
+                _getSystemAvInfo((IntPtr)p);
         }
 
         public void run()
         {
-            GetFunc<RunFunc>("retro_run")();
+            _run();
         }
 
         public void deinit()
         {
-            GetFunc<DeinitFunc>("retro_deinit")();
+            _deinit();
         }
 
         public void Dispose()
@@ -120,6 +149,13 @@ namespace Libretro.NET
             }
             _delegates.Clear();
         }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int EnvironmentBridge(uint cmd, IntPtr data);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void VideoRefreshBridge(IntPtr data, uint width, uint height, UIntPtr pitch);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate UIntPtr AudioSampleBatchBridge(IntPtr data, UIntPtr frames);
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void SetEnvironmentFunc(IntPtr cb);
@@ -138,11 +174,11 @@ namespace Libretro.NET
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void DeinitFunc();
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private unsafe delegate void GetSystemInfoFunc(retro_system_info* info);
+        private delegate void GetSystemInfoFunc(IntPtr info);
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private unsafe delegate byte LoadGameFunc(retro_game_info* game);
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private unsafe delegate void GetSystemAvInfoFunc(retro_system_av_info* av);
+        private delegate void GetSystemAvInfoFunc(IntPtr av);
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void RunFunc();
     }
--- Libretro.NET/RetroWrapper.cs
+++ Libretro.NET/RetroWrapper.cs
@@ -14,6 +14,8 @@ namespace Libretro.NET
         public double FPS { get; private set; }
         public double SampleRate { get; private set; }
         public retro_pixel_format PixelFormat { get; private set; }
+        public string SystemDirectory { get; set; } = ".";
+        public string SaveDirectory { get; set; } = ".";
 
         public delegate void OnFrameDelegate(byte[] frame, uint width, uint height);
         public OnFrameDelegate OnFrame;
@@ -23,6 +25,9 @@ namespace Libretro.NET
 
         public delegate bool OnCheckInputDelegate(uint port, uint device, uint index, uint id);
         public OnCheckInputDelegate OnCheckInput;
+
+        public delegate void OnLogDelegate(string level, string message);
+        public OnLogDelegate OnLog;
 
         public void LoadCore(string corePath)
         {
@@ -40,31 +45,52 @@ namespace Libretro.NET
 
         public bool LoadGame(string gamePath)
         {
-            var game = new retro_game_info
+            IntPtr systemPtr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(retro_system_info)));
+            try
             {
-                path = (sbyte*)Marshal.StringToHGlobalAnsi(gamePath),
-                size = (UIntPtr)new FileInfo(gamePath).Length
-            };
-
-            var system = new retro_system_info();
-            _core.get_system_info(ref system);
-
-            if (!system.need_fullpath)
+                _core.get_system_info(ref *(retro_system_info*)systemPtr);
+            }
+            catch
             {
-                game.data = (void*)Marshal.AllocHGlobal((int)game.size);
+                Marshal.FreeHGlobal(systemPtr);
+                throw;
+            }
+            bool needFullPath = ((retro_system_info*)systemPtr)->need_fullpath;
+            Marshal.FreeHGlobal(systemPtr);
+
+            IntPtr pathPtr = Marshal.StringToHGlobalAnsi(gamePath);
+            IntPtr dataPtr = IntPtr.Zero;
+            long fileSize = new FileInfo(gamePath).Length;
+
+            if (!needFullPath)
+            {
+                dataPtr = Marshal.AllocHGlobal((int)fileSize);
                 byte[] bytes = File.ReadAllBytes(gamePath);
-                Marshal.Copy(bytes, 0, (IntPtr)game.data, (int)game.size);
+                Marshal.Copy(bytes, 0, dataPtr, (int)fileSize);
             }
 
-            var result = _core.load_game(ref game);
+            IntPtr gamePtr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(retro_game_info)));
+            retro_game_info* g = (retro_game_info*)gamePtr;
+            g->path = (sbyte*)pathPtr;
+            g->data = (void*)dataPtr;
+            g->size = (UIntPtr)fileSize;
+            g->meta = null;
 
-            var av = new retro_system_av_info();
-            _core.get_system_av_info(ref av);
+            byte result = _core.load_game(ref *g);
 
-            Width = av.geometry.base_width;
-            Height = av.geometry.base_height;
-            FPS = av.timing.fps;
-            SampleRate = av.timing.sample_rate;
+            Marshal.FreeHGlobal(gamePtr);
+            Marshal.FreeHGlobal(pathPtr);
+            if (dataPtr != IntPtr.Zero)
+                Marshal.FreeHGlobal(dataPtr);
+
+            IntPtr avPtr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(retro_system_av_info)));
+            _core.get_system_av_info(ref *(retro_system_av_info*)avPtr);
+            retro_system_av_info* av = (retro_system_av_info*)avPtr;
+            Width = av->geometry.base_width;
+            Height = av->geometry.base_height;
+            FPS = av->timing.fps;
+            SampleRate = av->timing.sample_rate;
+            Marshal.FreeHGlobal(avPtr);
 
             return result == 1;
         }
@@ -79,35 +105,41 @@ namespace Libretro.NET
             switch (cmd)
             {
                 case RetroBindings.RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY:
-                {
-                    char** cb = (char**)data;
-                    *cb = (char*)Marshal.StringToHGlobalAnsi(".");
-                    return true;
-                }
+                    {
+                        char** cb = (char**)data;
+                        *cb = (char*)Marshal.StringToHGlobalAnsi(SystemDirectory ?? ".");
+                        return true;
+                    }
+                case RetroBindings.RETRO_ENVIRONMENT_GET_SAVE_DIRECTORY:
+                    {
+                        char** cb = (char**)data;
+                        *cb = (char*)Marshal.StringToHGlobalAnsi(SaveDirectory ?? SystemDirectory ?? ".");
+                        return true;
+                    }
                 case RetroBindings.RETRO_ENVIRONMENT_SET_PIXEL_FORMAT:
-                {
-                    PixelFormat = (retro_pixel_format)(*(byte*)data);
-                    return true;
-                }
+                    {
+                        PixelFormat = (retro_pixel_format)(*(byte*)data);
+                        return true;
+                    }
                 case RetroBindings.RETRO_ENVIRONMENT_GET_LOG_INTERFACE:
-                {
-                    retro_log_callback* cb = (retro_log_callback*)data;
-                    retro_log_printf_t logDel = Log;
-                    cb->log = _core.Register(logDel);
-                    return true;
-                }
+                    {
+                        retro_log_callback* cb = (retro_log_callback*)data;
+                        retro_log_printf_t logDel = Log;
+                        cb->log = _core.Register(logDel);
+                        return true;
+                    }
                 case RetroBindings.RETRO_ENVIRONMENT_GET_CAN_DUPE:
-                {
-                    *(bool*)data = true;
-                    return true;
-                }
+                    {
+                        *(bool*)data = true;
+                        return true;
+                    }
                 case RetroBindings.RETRO_ENVIRONMENT_SET_FRAME_TIME_CALLBACK:
-                {
-                    retro_frame_time_callback* cb = (retro_frame_time_callback*)data;
-                    retro_frame_time_callback_t timeDel = Time;
-                    cb->callback = _core.Register(timeDel);
-                    return true;
-                }
+                    {
+                        retro_frame_time_callback* cb = (retro_frame_time_callback*)data;
+                        retro_frame_time_callback_t timeDel = Time;
+                        cb->callback = _core.Register(timeDel);
+                        return true;
+                    }
                 default:
                     return false;
             }
@@ -168,8 +200,18 @@ namespace Libretro.NET
             return frames;
         }
 
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int _vsnprintf(sbyte* buffer, UIntPtr count, sbyte* format, IntPtr args);
+
         private void Log(retro_log_level level, sbyte* fmt)
         {
+            if (OnLog == null) return;
+            string levelStr = level == retro_log_level.RETRO_LOG_ERROR ? "ERROR" :
+                              level == retro_log_level.RETRO_LOG_WARN ? "WARN" :
+                              level == retro_log_level.RETRO_LOG_INFO ? "INFO" : "DEBUG";
+            string raw = Marshal.PtrToStringAnsi((IntPtr)fmt) ?? "";
+            string msg = raw.Contains("%") ? raw.Replace("%s", "?").Replace("%d", "?").Replace("%i", "?").Replace("%u", "?").Replace("%f", "?").Replace("%x", "?").TrimEnd('\n', '\r') : raw.TrimEnd('\n', '\r');
+            OnLog(levelStr, msg);
         }
 
         private void Time(long usec)
